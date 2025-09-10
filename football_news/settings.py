359481fe18f8788ee8f991a5d5b6bdfd7923b48a
Faishal Khoiriansyah Wicaksono <faishal30112005@gmail.com>
2025-09-10 15:15:48 +0700

    <pesan_commit>

--- football_news/settings.py
+++ football_news/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -148,3 +148,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://faishal-khoiriansyah-footballnews.pbp.cs.ui.ac.id/"
+]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import NewsForm
+from main.models import News
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
 def show_main(request):
+    news_list = News.objects.all()
     context = {
-        'npm': '2406436335',     # ganti dengan NPM kamu
-        'name': 'Faishal Khoiriansyah Wicaksono',  # ganti dengan nama kamu
-        'class': 'PBP D'         # ganti dengan kelas kamu
+        'npm' : '2406436335',
+        'name': 'Faishal Khoiriansyah Wicaksono',
+        'class': 'PBP D',
+        'news_list': news_list
     }
-
     return render(request, "main.html", context)
+
+def create_news(request):
+    form = NewsForm(request.POST or None)
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    return render(request, "create_news.html", {'form': form})
+
+def show_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.increment_views()
+    return render(request, "news_detail.html", {'news': news})
+
+# Semua data
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize("xml", news_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+# Data berdasarkan ID
+def show_xml_by_id(request, news_id):
+    try:
+        news_item = News.objects.get(pk=news_id)
+        xml_data = serializers.serialize("xml", [news_item])
+        return HttpResponse(xml_data, content_type="application/xml")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
+
+def show_json_by_id(request, news_id):
+    try:
+        news_item = News.objects.get(pk=news_id)
+        json_data = serializers.serialize("json", [news_item])
+        return HttpResponse(json_data, content_type="application/json")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
