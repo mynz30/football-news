f79d0164db757654f3cf5050ffbc6c587cc999f4
Faishal Khoiriansyah Wicaksono <faishal30112005@gmail.com>
2025-10-01 14:41:04 +0700

    Tutorial 6

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import edit_news
 from main.views import delete_news
+from main.views import add_news_entry_ajax
 
 app_name = 'main'
 
@@ -27,4 +28,5 @@ urlpatterns = [
 
     path('news/<uuid:id>/edit', edit_news, name='edit_news'),
     path('news/<uuid:id>/delete', delete_news, name='delete_news'),
+    path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
 ]
